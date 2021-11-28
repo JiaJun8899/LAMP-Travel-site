5f7771f98a6da2566a37341ab6926466a6ac71de
Chen JiaJun <90199318+JiaJun8899@users.noreply.github.com>
2021-11-28 15:07:20 +0800

    Update test.php

--- test.php
+++ test.php
@@ -18,8 +18,8 @@ $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
 $mail->isSMTP(); //Send using SMTP
 $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
 $mail->SMTPAuth = true; //Enable SMTP authentication
-$mail->Username = 'ict1004.allons@gmail.com'; //SMTP username
-$mail->Password = 's9923767h'; //SMTP password
+$mail->Username = ; //SMTP username
+$mail->Password = ; //SMTP password
 // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
 $mail->Port = 587; //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS 
 try {
